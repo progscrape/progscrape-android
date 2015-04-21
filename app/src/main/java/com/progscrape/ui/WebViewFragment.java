1ad6fdfa9fa5cecbf93fbe8caf2764745f2ca14a
Matt Mastracci <matthew@mastracci.com>
2015-04-21 16:58:57 -0600

    Improve browser experience

--- app/src/main/java/com/progscrape/ui/WebViewFragment.java
+++ app/src/main/java/com/progscrape/ui/WebViewFragment.java
@@ -7,7 +7,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.progscrape.R;
 import com.progscrape.app.data.Story;
@@ -17,15 +21,20 @@ import butterknife.InjectView;
 
 public class WebViewFragment extends Fragment {
     private String href;
+    private String titleText;
 
     @Override
     public void setArguments(Bundle args) {
         super.setArguments(args);
         href = (String) args.get("href");
+        titleText = (String) args.get("title");
     }
 
     @InjectView(R.id.browser)
     WebView browser;
+
+    @InjectView(R.id.title)
+    TextView title;
 
     @Nullable
     @Override
@@ -37,13 +46,24 @@ public class WebViewFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         ButterKnife.inject(this, getView());
+        WebSettings settings = browser.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setSupportZoom(true);
+        settings.setDisplayZoomControls(false);
+        settings.setBuiltInZoomControls(true);
+        settings.setUseWideViewPort(true);
+        browser.setWebChromeClient(new WebChromeClient());
+        browser.setWebViewClient(new WebViewClient());
         browser.loadUrl(href);
+
+        title.setText(titleText);
     }
 
     public static WebViewFragment newInstance(Story story) {
         WebViewFragment fragment = new WebViewFragment();
         Bundle args = new Bundle();
         args.putString("href", story.getHref());
+        args.putString("title", story.getTitle());
         fragment.setArguments(args);
         return fragment;
     }
